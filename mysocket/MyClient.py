b207c5d6033d8e84fd775fbdbbbf262f7d0bad38
venkatram64 <venkat.veerareddy@hotmail.com>
2018-10-28 09:05:38 +0530

    init commit

--- mysocket/MyClient.py
+++ mysocket/MyClient.py
@@ -8,13 +8,12 @@ class MyClient:
         client_socket.connect(("127.0.0.1", 4545))
         while True:
             client_msg = input("Enter a message to send server:\n ")
-            if client_msg == "bye":
-                client_socket.close()
-                break
             client_socket.send(client_msg.encode("utf8"))
             msg_from_server = client_socket.recv(4096)
             print("Received msg from server is {} ".format(msg_from_server.decode("utf8")))
-
+            if client_msg == "quit":
+                break
+        client_socket.close()
 
 
 if __name__ == "__main__":
