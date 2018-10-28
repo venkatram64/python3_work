fddc7cb95323b79a63620d84da3f89434bbd0f69
venkatram64 <venkat.veerareddy@hotmail.com>
2018-10-28 10:26:09 +0530

    init commit

--- mysocket/MyClient.py
+++ mysocket/MyClient.py
@@ -1,9 +1,9 @@
-
 import socket
+from threading import Thread
 
 class MyClient:
 
-    def start_client(self):
+    def start_client2(self):
         client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         client_socket.connect(("127.0.0.1", 4545))
         while True:
@@ -14,6 +14,31 @@ class MyClient:
             if client_msg == "quit":
                 break
         client_socket.close()
+
+    def start_client(self):
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket.connect(("127.0.0.1", 4545))
+        receive_thread = Thread(target=self.receive, args=(client_socket,))
+        receive_thread.start()
+        send_thread = Thread(target=self.send, args=(client_socket,))
+        send_thread.start()
+
+    def receive(self, conn, MAX_BUFFER_SIZE=4096):
+        while True:
+            msg = conn.recv(MAX_BUFFER_SIZE).decode("utf8")
+            if msg == "quit":
+                conn.close()
+                break
+            if not msg:
+                break
+            print(msg)
+
+    def send(self, conn):
+        while True:
+            msg = input("Enter: ")
+            conn.send(bytes(msg, "utf8"))
+            if msg == "quit":
+                break
 
 
 if __name__ == "__main__":
